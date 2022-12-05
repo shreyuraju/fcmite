41569eeae1ec7fea36b48f270c39f9ded416e28a
shreyuraju <shreyu.raju@gmail.com>
2022-12-05 21:58:40 +0530

    registering to database with proper USN length

--- app/src/main/java/com/mite/mitefc/Register.java
+++ app/src/main/java/com/mite/mitefc/Register.java
@@ -1,5 +1,6 @@
 package com.mite.mitefc;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.PendingIntent;
@@ -13,13 +14,24 @@ import android.nfc.tech.Ndef;
 import android.nfc.tech.NdefFormatable;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.Locale;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Register extends AppCompatActivity {
 
@@ -27,6 +39,10 @@ public class Register extends AppCompatActivity {
     private EditText registerText;
     private NfcAdapter nfcAdapter;
     private boolean isWrite = true;
+    private final String regex = "";
+
+    DatabaseReference reference;
+    Pattern pattern;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +53,10 @@ public class Register extends AppCompatActivity {
 
         nfcAdapter = NfcAdapter.getDefaultAdapter(this);
         registerText = findViewById(R.id.registerText);
+        reference = FirebaseDatabase.getInstance().getReference();
+
+        pattern = Pattern.compile(regex);
+
 
     }
 
@@ -44,26 +64,59 @@ public class Register extends AppCompatActivity {
     protected void onNewIntent(Intent intent) {
         super.onNewIntent(intent);
         Tag tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+
         if (tag != null) {
             if (isWrite) {
                 String messageToWrite = registerText.getText().toString().toUpperCase(Locale.ROOT);
-
-                if (messageToWrite != null && (!TextUtils.equals(messageToWrite, "null")) && (!TextUtils.isEmpty(messageToWrite))) {
-                    NdefRecord record = NdefRecord.createMime(messageToWrite, messageToWrite.getBytes());
-                    NdefMessage message = new NdefMessage(new NdefRecord[]{record});
-
-                    if (writeTag(tag, message)) {
-                        Boolean writeToDb = wirteToDB(messageToWrite);
-                        Toast.makeText(getApplicationContext(), "Successfully Registered", Toast.LENGTH_SHORT).show();
-                        finish();
+                Matcher matcher = pattern.matcher(messageToWrite);
+                //matcher.matches()
+                    if (messageToWrite.length() < 10) {
+                        registerText.setError("Enter Proper reg USN");
                     } else {
-                        Toast.makeText(getApplicationContext(), "Error Registering", Toast.LENGTH_SHORT).show();
+                        if (messageToWrite != null && (!TextUtils.equals(messageToWrite, "null")) && (!TextUtils.isEmpty(messageToWrite))) {
+                            NdefRecord record = NdefRecord.createMime(messageToWrite, messageToWrite.getBytes());
+                            NdefMessage message = new NdefMessage(new NdefRecord[]{record});
+                            if (writeData(messageToWrite)) {
+                                if (writeTag(tag, message)) {
+                                    Boolean writeToDb = wirteToDB(messageToWrite);
+                                    if(writeToDb)
+                                        Toast.makeText(getApplicationContext(), "Successfully Registered", Toast.LENGTH_SHORT).show();
+                                    finish();
+                                } else {
+                                    Toast.makeText(getApplicationContext(), "Error Registering", Toast.LENGTH_SHORT).show();
+                                }
+                            } else {
+                                Toast.makeText(getApplicationContext(), "Error Registering", Toast.LENGTH_SHORT).show();
+                            }
+                        } else {
+                            registerText.setError("Please enter the text to write");
+                        }
                     }
-                } else {
-                    registerText.setError("Please enter the text to write");
-                }
             }
         }
+    }
+
+    private boolean writeData(String messageToWrite) {
+        DatabaseReference databaseReference = reference.child("users").child(messageToWrite).push();
+        Map map = new HashMap();
+        map.put("USN", messageToWrite);
+        map.put("balance", 0);
+
+        databaseReference.updateChildren(map).addOnCompleteListener(new OnCompleteListener() {
+            @Override
+            public void onComplete(@NonNull Task task) {
+                if(task.isSuccessful()) {
+                    Toast.makeText(Register.this, "Data is recorded in DB", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Log.d("ERROR :", e.getMessage());
+            }
+        });
+
+        return true;
     }
 
     private Boolean wirteToDB(String messageToWrite) {
