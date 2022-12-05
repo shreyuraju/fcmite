d76cafd4d686e1475e612c4a90cb39b532c14065
shreyuraju <shreyu.raju@gmail.com>
2022-12-05 15:18:36 +0530

    Checking NFC Hardware is present or not

--- app/src/main/java/com/mite/mitefc/Home.java
+++ app/src/main/java/com/mite/mitefc/Home.java
@@ -3,18 +3,36 @@ package com.mite.mitefc;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.nfc.NdefMessage;
+import android.nfc.NdefRecord;
 import android.nfc.NfcAdapter;
+import android.nfc.NfcManager;
+import android.nfc.Tag;
+import android.nfc.tech.MifareClassic;
+import android.nfc.tech.Ndef;
+import android.nfc.tech.NdefFormatable;
+import android.nfc.tech.NfcA;
 import android.os.Bundle;
+import android.os.IBinder;
+import android.os.Parcel;
+import android.os.Parcelable;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.IOException;
 
 
 public class Home extends AppCompatActivity {
 
-    private NfcAdapter nfcAdapter;
+    private NfcAdapter nfcAdapter, adapter;
     private TextView NFCText;
     public static final String TAG = Home.class.getSimpleName();
 
@@ -24,10 +42,183 @@ public class Home extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-
+        checkForNFCHardware();
+        nfcAdapter = NfcAdapter.getDefaultAdapter(this);
+        NFCText = findViewById(R.id.TAGdata);
 
     }
 
+    private void checkForNFCHardware() {
+        Context context=this;
+        NfcManager nfcManager = (NfcManager) context.getSystemService(Context.NFC_SERVICE);
+        adapter = nfcManager.getDefaultAdapter();
+        if (adapter != null && !adapter.isEnabled()) {
+            Toast.makeText(getApplicationContext(), "Need to Enable NFC", Toast.LENGTH_SHORT).show();
+        } else if (adapter != null && adapter.isEnabled()) {
+            Toast.makeText(getApplicationContext(), "NFC available", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(getApplicationContext(), "NFC Hardware not detected", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    //on pause and on resume of NFC Activity
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if(nfcAdapter != null) nfcAdapter.disableForegroundDispatch(this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        IntentFilter tagDetected = new IntentFilter(NfcAdapter.ACTION_TAG_DISCOVERED);
+        IntentFilter ndefDetected = new IntentFilter(NfcAdapter.ACTION_NDEF_DISCOVERED);
+        IntentFilter techDetected = new IntentFilter(NfcAdapter.ACTION_TECH_DISCOVERED);
+        IntentFilter[] nfcIntentFilter = new IntentFilter[]{techDetected, tagDetected, ndefDetected};
+
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, new Intent(this, getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0);
+        if (nfcAdapter != null)
+            nfcAdapter.enableForegroundDispatch(this, pendingIntent, nfcIntentFilter, null);
+    }
+
+    //reading
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        Tag tag = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+        patchTag(tag);
+        if (tag != null) {
+            readFromNFC(tag, intent);
+        }
+    }
+
+    private void readFromNFC(Tag tag, Intent intent) {
+        try {
+            Ndef ndef = Ndef.get(tag);
+            if (ndef != null) {
+                ndef.connect();
+                NdefMessage ndefMessage = ndef.getNdefMessage();
+
+                if (ndefMessage != null) {
+                    /*String message = new String(ndefMessage.getRecords()[0].getPayload());
+                    Log.d(TAG, "NFC found.. "+"readFromNFC: "+message );
+                    tvNFCMessage.setText(message);*/
+
+                    Parcelable[] messages = intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES);
+
+                    if (messages != null) {
+                        NdefMessage[] ndefMessages = new NdefMessage[messages.length];
+                        for (int i = 0; i < messages.length; i++) {
+                            ndefMessages[i] = (NdefMessage) messages[i];
+                        }
+                        NdefRecord record = ndefMessages[0].getRecords()[0];
+
+                        byte[] payload = record.getPayload();
+                        String text = new String(payload);
+                        NFCText.setText(text);
+                        Log.e("tag", "vahid  -->  " + text);
+                        ndef.close();
+
+                    }
+
+                } else {
+                    Toast.makeText(this, "Not able to read from NFC, Please try again...", Toast.LENGTH_LONG).show();
+
+                }
+            } else {
+                NdefFormatable format = NdefFormatable.get(tag);
+                if (format != null) {
+                    try {
+                        format.connect();
+                        NdefMessage ndefMessage = ndef.getNdefMessage();
+
+                        if (ndefMessage != null) {
+                            String message = new String(ndefMessage.getRecords()[0].getPayload());
+                            Log.d(TAG, "NFC found.. " + "readFromNFC: " + message);
+                            NFCText.setText(message);
+                            ndef.close();
+                        } else {
+                            Toast.makeText(this, "Not able to read from NFC, Please try again...", Toast.LENGTH_LONG).show();
+
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                } else {
+                    Toast.makeText(this, "NFC is not readable", Toast.LENGTH_LONG).show();
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private Tag patchTag(Tag oTag) {
+        if (oTag == null)
+            return null;
+
+        String[] sTechList = oTag.getTechList();
+
+        Parcel oParcel, nParcel;
+
+        oParcel = Parcel.obtain();
+        oTag.writeToParcel(oParcel, 0);
+        oParcel.setDataPosition(0);
+
+        int len = oParcel.readInt();
+        byte[] id = null;
+        if (len >= 0) {
+            id = new byte[len];
+            oParcel.readByteArray(id);
+        }
+        int[] oTechList = new int[oParcel.readInt()];
+        oParcel.readIntArray(oTechList);
+        Bundle[] oTechExtras = oParcel.createTypedArray(Bundle.CREATOR);
+        int serviceHandle = oParcel.readInt();
+        int isMock = oParcel.readInt();
+        IBinder tagService;
+        if (isMock == 0) {
+            tagService = oParcel.readStrongBinder();
+        } else {
+            tagService = null;
+        }
+        oParcel.recycle();
+
+        int nfca_idx = -1;
+        int mc_idx = -1;
+
+        for (int idx = 0; idx < sTechList.length; idx++) {
+            if (sTechList[idx] == NfcA.class.getName()) {
+                nfca_idx = idx;
+            } else if (sTechList[idx] == MifareClassic.class.getName()) {
+                mc_idx = idx;
+            }
+        }
+
+        if (nfca_idx >= 0 && mc_idx >= 0 && oTechExtras[mc_idx] == null) {
+            oTechExtras[mc_idx] = oTechExtras[nfca_idx];
+        } else {
+            return oTag;
+        }
+
+        nParcel = Parcel.obtain();
+        nParcel.writeInt(id.length);
+        nParcel.writeByteArray(id);
+        nParcel.writeInt(oTechList.length);
+        nParcel.writeIntArray(oTechList);
+        nParcel.writeTypedArray(oTechExtras, 0);
+        nParcel.writeInt(serviceHandle);
+        nParcel.writeInt(isMock);
+        if (isMock == 0) {
+            nParcel.writeStrongBinder(tagService);
+        }
+        nParcel.setDataPosition(0);
+        Tag nTag = Tag.CREATOR.createFromParcel(nParcel);
+        nParcel.recycle();
+        return nTag;
+    }
+
+    //POPUP option menu
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = new MenuInflater(this);
